deff5d96eb779483de73c0c305c481d3feaa3bbc
anggirakasiwi <anggi.16411050@student.ubl.ac.id>
2019-03-16 20:28:01 +0700

    TugasMobile1

--- app/src/main/java/com/example/tugas1/MainActivity.java
+++ app/src/main/java/com/example/tugas1/MainActivity.java
@@ -14,19 +14,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txtnama = (TextView) findViewById(R.id.txtnama);
-        txtnama.setText("Anggi Raka Siwi");
+        txtnama.setText("Nama   : Anggi Raka Siwi");
 
         txtnpm = (TextView) findViewById(R.id.txtnpm);
-        txtnpm.setText("16411050");
+        txtnpm.setText("NPM : 16411050");
 
         txtttl = (TextView) findViewById(R.id.txtttl);
-        txtttl.setText("21 Juli 1998");
+        txtttl.setText("TTL : Bandar Lampung, 21 Juli 1998");
 
         txtalamat = (TextView) findViewById(R.id.txtalamat);
-        txtalamat.setText("Jln. Patimura Gang Singgalang No.19 TBU");
+        txtalamat.setText("Alamat   : Jln. Patimura Gang Singgalang No.19 TBU");
 
         txthobi = (TextView) findViewById(R.id.txthobi);
-        txthobi.setText("Mendengarkan KPOP, Menonton Drakor, Baca Novel");
+        txthobi.setText("Hobi   : Mendengarkan KPOP, Menonton Drakor");
 
     }
 }
